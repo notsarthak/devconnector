8e329b9dc755fc6028b50126d3a7bb77413b9b9f
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-08-16 17:00:09 +0530

    Started by creatung dumb components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
