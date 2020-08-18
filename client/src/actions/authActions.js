e6093f56ea26f86c5041503b251d03015323ac4d
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-08-19 00:39:29 +0530

    Error reducer, dispatching actions after waiting for server's response, redirecting from action creators, componentWillRecieveProps hook

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,14 @@
-import { TEST_DISPATCH } from "./types";
+import { GET_ERRORS } from "./types";
+import axios from "axios";
 
-export const registerUser = (userData) => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData,
-  };
+export const registerUser = (userData, history) => async(dispatch) => {
+  try{
+    const res = await axios.post("/api/users", userData);
+    return history.push('/login');
+  }catch(e){
+    return dispatch({
+        type: GET_ERRORS,
+        payload: e.response.data.errors
+    })
+  }
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { withRouter } from "react-router-dom";
 import classnames from "classnames";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
@@ -23,6 +23,14 @@ class Register extends Component {
       [e.target.name]: e.target.value,
     });
   }
+  componentWillReceiveProps(nextProps) {
+    const { errors } = nextProps;
+    if (errors) {
+      this.setState({
+        errors: errors,
+      });
+    }
+  }
   onSubmit(e) {
     //this is where we'll register the user and its gonna go thru redux. Right now, just logging whatev to be sent to backend, on the console
     e.preventDefault();
@@ -30,24 +38,12 @@ class Register extends Component {
       ...this.state,
     };
     delete newUser.errors; //delete property from object
-    /*axios
-      .post("/api/users", newUser)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((e) =>{
-        this.setState({ errors: e.response.data.errors });
-        console.log(e.response.data.errors)
-      });*/
-    this.props.registerUser(newUser);  
-    console.log(newUser);
+    this.props.registerUser(newUser, this.props.history);
   }
   render() {
     const { errors } = this.state;
-    const { user } = this.props.auth;
     return (
       <div className="register">
-      {user?user.name:null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -60,28 +56,35 @@ class Register extends Component {
                   <input
                     type="text"
                     className={classnames("form-control form-control-lg", {
-                      "is-invalid": errors.find(err=> err.param==='name'),
+                      "is-invalid": errors.find((err) => err.param === "name"),
                     })}
                     placeholder="Name"
                     value={this.state.name}
                     name="name"
                     onChange={this.onChange}
-                  />  
-                {user.name?(user.name):(null)}                 
-                {errors.find(err=> err.param==='name') && <div className="invalid-feedback">{errors.find(err=>err.param==='name').msg}</div>}
+                  />
+                  {errors.find((err) => err.param === "name") && (
+                    <div className="invalid-feedback">
+                      {errors.find((err) => err.param === "name").msg}
+                    </div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
-                    className={classnames("form-control form-control-lg",{
-                      'is-invalid': errors.find(err=> err.param==='email')
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.find((err) => err.param === "email"),
                     })}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChange}
                   />
-                  {errors.find(err=> err.param==='email') && <div className="invalid-feedback">{errors.find(err=>err.param==='email').msg}</div>}
+                  {errors.find((err) => err.param === "email") && (
+                    <div className="invalid-feedback">
+                      {errors.find((err) => err.param === "email").msg}
+                    </div>
+                  )}
                   <small className="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
@@ -90,28 +93,40 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="password"
-                    className={classnames("form-control form-control-lg",{
-                      'is-invalid': errors.find(err=> err.param==='password')
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.find(
+                        (err) => err.param === "password"
+                      ),
                     })}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
-                {errors.find(err=> err.param==='password') && <div className="invalid-feedback">{errors.find(err=>err.param==='password').msg}</div>}
+                  {errors.find((err) => err.param === "password") && (
+                    <div className="invalid-feedback">
+                      {errors.find((err) => err.param === "password").msg}
+                    </div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className={classnames("form-control form-control-lg",{
-                      'is-invalid': errors.find(err=> err.param==='password2')
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.find(
+                        (err) => err.param === "password2"
+                      ),
                     })}
                     placeholder="Re-enter Password"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.onChange}
                   />
-                  {errors.find(err=> err.param==='password2') && <div className="invalid-feedback">{errors.find(err=>err.param==='password2').msg}</div>}
+                  {errors.find((err) => err.param === "password2") && (
+                    <div className="invalid-feedback">
+                      {errors.find((err) => err.param === "password2").msg}
+                    </div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
@@ -125,13 +140,15 @@ class Register extends Component {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-}
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.array.isRequired,
+};
 
-const  mapStateToProps = (state) => {
+const mapStateToProps = (state) => {
   return {
-    auth: state.auth
-  }
-}
+    auth: state.auth,
+    errors: state.errors,
+  };
+};
 
-export default connect(mapStateToProps, { registerUser })(Register);
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
